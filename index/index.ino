2ccf0da8656157fa1fd832380bbb8a71c7252e67
htcryme <56440776+htcryme@users.noreply.github.com>
2020-12-19 15:52:01 +0100

    update

--- index/index.ino
+++ index/index.ino
@@ -45,6 +45,8 @@ void setup() {
   }
 
   pixels.begin();
+
+  randomSeed(analogRead(0));
 }
 
 void loop() {
@@ -63,11 +65,33 @@ void loop() {
 
 
 bool on = true;
+bool runAnimation = false;
 float hue = 0;
+float hueStep = 1;
 float light = 0.5;
 float lightStep = 0.0625;
 float saturation = 0;
 float saturationStep = 0.0625;
+
+int frame = 0;
+
+int state = 0;
+
+int pulsateColorState = 0;
+int rainbowColorState = 1;
+int flashColorState = 2;
+int blinkColorState = 3;
+int movingLeftPointState = 4;
+int movingRightPointState = 5;
+int movingZickZackPointState = 6;
+int movingLeftStripeState = 7;
+int movingRightStripeState = 8;
+int movingZickZackStripeState = 9;
+int movingInStripeState = 10;
+int movingOutStripeState = 11;
+int movingRainbowState = 12;
+
+int states = 13;
 
 
 void processRemote(int num) {
@@ -83,7 +107,7 @@ void processRemote(int num) {
       hue += 360;
     }
   } else if (num == PAUSE) {
-
+    runAnimation = !runAnimation;
   } else if (num == RIGHT) {
     hue++;
     if (hue > 360) {
@@ -96,53 +120,239 @@ void processRemote(int num) {
   } else if (num == UP) {
     saturation = min(1, saturation + saturationStep);
   } else if (num == ZERO) {
-
+    hue = 0;
+    saturation = 0;
+    light = 1;
   } else if (num == EQ) {
-
+    state = (state + 1) % states;
   } else if (num == REPT) {
 
-  } else if (num == ONE) {
-
-  } else if (num == TWO) {
-
-  } else if (num == THREE) {
-
-  } else if (num == FOUR) {
-
-  } else if (num == FIVE) {
-
-  } else if (num == SIX) {
-
-  } else if (num == SEVEN) {
-
-  } else if (num == EIGHT) {
-
-  } else if (num == NINE) {
-
+  } else if (num == ONE) {//rot
+    hue = 0;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == TWO) {//orange
+    hue = 30;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == THREE) {//gelb
+    hue = 60;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == FOUR) {//grün
+    hue = 120;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == FIVE) {//cyan
+    hue = 180;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == SIX) {//blau
+    hue = 220;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == SEVEN) {//lila
+    hue = 280;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == EIGHT) {//pink
+    hue = 315;
+    saturation = 1;
+    light = 0.5;
+  } else if (num == NINE) {//grau
+    hue = 0;
+    saturation = 0;
+    light = 0.25;
   } else if (num == 0) {
-
+    
   }
 
+  
+  
   lastPressed = num;
 }
 
 
+
+void color(int * col, float h, float s, float l) {
+  float r = max(0, (cos(h / 360 * 2 * PI) + 0.5) / 1.5);
+  float g = max(0, (cos((h + 120) / 360 * 2 * PI) + 0.5) / 1.5);
+  float b = max(0, (cos((h + 240) / 360 * 2 * PI) + 0.5) / 1.5);
+
+  if (l < 0.5) {
+    r = r * 2 * l;
+    g = g * 2 * l;
+    b = b * 2 * l;
+  } else {
+    r = min(1, (r + light - 0.5) * 2 * l);
+    g = min(1, (g + light - 0.5) * 2 * l);
+    b = min(1, (b + light - 0.5) * 2 * l);
+  }
+
+  float average = (r + g + b) / 3;
+  float s_ = sqrt(s);
+  r = r * s_ + average * (1 - s_);
+  g = g * s_ + average * (1 - s_);
+  b = b * s_ + average * (1 - s_);
+
+  col[0] = 255 * r;
+  col[1] = 255 * g;
+  col[2] = 255 * b;
+}
+
+
 void updateLEDs() {
-  hue += 1;
-  if (hue > 360) {
+
+
+  if (on) {
+    if (runAnimation) {
+      frame++;
+      if(state == pulsateColorState){
+        pulsateColor();
+      }else if(state == rainbowColorState){
+        rainbowColor();
+      }else if(state == flashColorState){
+        flashColor();
+      }else if(state == blinkColorState){
+        blinkColor();
+      }else if(state == movingLeftPointState){
+        movingLeftPoint();
+      }else if(state == movingRightPointState){
+        movingRightPoint();
+      }else if(state == movingZickZackPointState){
+        movingZickZackPoint();
+      }else if(state == movingLeftStripeState){
+        movingLeftStripe();
+      }else if(state == movingRightStripeState){
+        movingRightStripe();
+      }else if(state == movingZickZackStripeState){
+        movingZickZackStripe();
+      }else if(state == movingInStripeState){
+        movingInStripe();
+      }else if(state == movingOutStripeState){
+        movingOutStripe();
+      }else if(state == movingRainbowState){
+        movingRainbow();
+      }
+    } else {
+      flatColor();
+    }
+  } else {
+    pixels.clear();
+  }
+  
+  pixels.show();
+}
+
+
+void flatColor() {
+  int col[3];
+  color(col, hue, saturation, light);
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(col[0], col[1], col[2]));
+  }
+}
+
+void pulsateColor() {
+  saturation = (sin(frame / 60.0 * PI) + 1) / 4 + 0.5;
+
+  Serial.println(saturation);
+  
+  int col[3];
+  color(col, hue, saturation, light);
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(col[0], col[1], col[2]));
+  }
+}
+
+void rainbowColor() {
+  hue += hueStep;
+  while (hue >= 360) {
     hue -= 360;
   }
 
-  float r_ = 255 * (cos(hue / 360 * 2 * PI) + 1) / 2 * light;
-  float g_ = 255 * (cos((hue + 120) / 360 * 2 * PI) + 1) / 2 * light;
-  float b_ = 255 * (cos((hue + 240) / 360 * 2 * PI) + 1) / 2 * light;
+  int col[3];
+  color(col, hue, saturation, light);
 
-  int r = r_ * (saturation) + (g_ + b_) / 2 * (1 - saturation);
-  int g = g_ * (saturation) + (r_ + b_) / 2 * (1 - saturation);
-  int b = b_ * (saturation) + (r_ + g_) / 2 * (1 - saturation);
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(col[0], col[1], col[2]));
+  }
+}
 
+void flashColor() {
+  if (frame % 10 < 5) {
+    int col[3];
+    color(col, hue, saturation, light);
 
-  pixels.setPixelColor(1, pixels.Color(r, g, b));
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(col[0], col[1], col[2]));
+    }
+  } else {
+    pixels.clear();
+  }
+}
 
-  pixels.show();
+void blinkColor() {
+  int col[3];
+  color(col, hue, saturation, light);
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(col[0], col[1], col[2]));
+  }
+
+  for(int i=0;i<NUMPIXELS/3;i++){
+    pixels.setPixelColor(random(0, NUMPIXELS), pixels.Color(0, 0, 0));
+  }
+
+  delay(100);
+}
+
+void movingLeftPoint(){
+  pixels.clear();
+  
+  int col[3];
+  color(col, hue, saturation, light);
+
+  pixels.setPixelColor(frame % NUMPIXELS, pixels.Color(col[0], col[1], col[2]));
+  delay(100);
+}
+
+void movingRightPoint(){
+  pixels.clear();
+  
+  int col[3];
+  color(col, hue, saturation, light);
+
+  pixels.setPixelColor(NUMPIXELS - frame % NUMPIXELS, pixels.Color(col[0], col[1], col[2]));
+  delay(100);
+}
+
+void movingZickZackPoint(){
+  
+}
+
+void movingLeftStripe(){
+  
+}
+
+void movingRightStripe(){
+  
+}
+
+void movingZickZackStripe(){
+  
+}
+
+void movingInStripe(){
+  
+}
+
+void movingOutStripe(){
+  
+}
+
+void movingRainbow(){
+  
 }
